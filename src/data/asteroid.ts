aac440143a32d7fce9c562b73f281a07b63bd271
richard.kolisek <richard.kolisek@eskerpartners.com>
2022-12-05 15:41:29 +0100

    add asteroid

--- src/data/asteroid.ts
+++ src/data/asteroid.ts
@@ -3,13 +3,20 @@ import {Scene} from "./scene";
 
 export class Asteroid
 {
-    private _lines: Line[];
-
-    constructor(vertCount: number, scene: Scene)
+    constructor(centerX: number, centerY: number, vertCount: number, radius: number, scene: Scene)
     {
         for (let i = 0; i < vertCount; i++)
         {
-            this._lines.push()
+            const angleP1 = ((Math.PI * 2) / vertCount) * i;
+            const angleP2 = ((Math.PI * 2) / vertCount) * (i + 1);
+
+            const line = new Line(
+                centerX + Math.sin(angleP1) * radius,
+                centerY + Math.cos(angleP1) * radius,
+                centerX + Math.sin(angleP2) * radius,
+                centerY + Math.cos(angleP2) * radius);
+            line.emmiting = true;
+            scene.lines.push(line);
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Scene } from "./data/scene";
 import { MainRenderer } from "./renderer/main_renderer";
+import {Asteroid} from "./data/asteroid";
 
 const canvas = <HTMLCanvasElement> document.getElementById("gfx-main");
 let scene = new Scene();
 
 scene.setPlayerPos(400, 300);
 scene.setRayCount(20000);
-scene.setReflectionCount(3)
+scene.setReflectionCount(3);
 
 canvas.addEventListener("mousemove", e =>
 {
@@ -20,5 +21,8 @@ let line = scene.addLine(500, 100, 600, 200);
 
 let line3 = scene.addLine(400, 100, 400, 200);
 line3.emmiting = true;
+
+new Asteroid(400, 300, 20, 10, scene);
+new Asteroid(300, 300, 20, 10, scene);
 
 new MainRenderer(canvas, scene).initialize();
